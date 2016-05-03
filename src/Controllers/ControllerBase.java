1ff0f42123dc5b79d995f16b82a9fc7a03ccf859
Dubouski <asxing@rambler.ru>
2016-05-03 03:43:45 +0300

    add CheckBoxEditor

--- src/Controllers/ControllerBase.java
+++ src/Controllers/ControllerBase.java
@@ -1,6 +1,8 @@
 package Controllers;
 
 import Game.ScenesLoader;
+import javafx.scene.control.CheckBox;
+import javafx.event.ActionEvent;
 
 public abstract class ControllerBase {
     protected ScenesLoader loader;
@@ -8,6 +10,19 @@ public abstract class ControllerBase {
     public void setLoader(ScenesLoader loader) {
         this.loader = loader;
         loader.changeResolutionScreen(isFullScreen);
+    }
+
+    /*
+    * Add CheckBoxScreen edit
+    * */
+    public void editBoxSound(ActionEvent event) {
+        System.out.println("CheckBoxSound!");
+    }
+    /*
+    * Add CheckBoxScreen edit
+    * */
+    public void editBoxScreen(ActionEvent event) {
+        System.out.println("CheckBoxScreen!");
     }
 
     public static boolean isFullScreen = true;
